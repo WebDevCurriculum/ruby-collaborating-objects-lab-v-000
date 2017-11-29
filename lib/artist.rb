af18f886d17b959dd11910324744d676388c14bc
charityyoungblood <youngblood.charity@gmail.com>
2017-11-28 19:37:59 -0800

    added comments on how to approach code; added require pry

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+require 'pry'
+# create class Artist 
+# create attr_accessor :name 
+# create instance variable to keep track of songs
+# create initialize method with argument (name) 
+
+# create songs method - keeps track of songs  
+
+# create save method 
+
+# create class method for .find_or_create_by_name
+# this method creates a new instance of an Artist if none exists 
+
+# create print_songs method 
