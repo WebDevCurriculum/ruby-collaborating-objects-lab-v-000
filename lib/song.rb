86f8454ccdf990f1943f0f7a1c5e5a0f6233bf5c
charityyoungblood <youngblood.charity@gmail.com>
2017-11-28 19:45:44 -0800

    added attr_accessors, intialize method to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
 require 'pry'
 
 # create class Song
-
+class Song
 # create attr_accessor :name, :artist
+  attr_accessor :name, :artist
+  def initialize(name)
 # initialize with name 
-
+  end 
 # create class method .new_by_filename
 
+end 
