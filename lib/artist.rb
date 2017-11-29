12fde0483efc480b5a97782924829d53052d4ce9
charityyoungblood <youngblood.charity@gmail.com>
2017-11-28 19:44:24 -0800

    added comments for how to approach code in song.rb

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,12 +4,15 @@ class Artist
 # create attr_accessor :name 
   attr_accessor :name
 # create instance variable to keep track of songs
-  @songs
+  @songs = []
 # create initialize method with argument (name) 
   def initialize(name)
+    @name = name
   end 
 # create songs method - keeps track of songs  
-
+  def songs
+    @songs
+  end 
 # create save method 
 
 # create class method for .find_or_create_by_name
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+# create class Song
+
+# create attr_accessor :name, :artist
+# initialize with name 
+
+# create class method .new_by_filename
+
