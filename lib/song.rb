f540451a31d4476bec48cae84dd7529a14f5d2bb
charityyoungblood <youngblood.charity@gmail.com>
2017-11-28 19:48:04 -0800

    added class method to song.rb

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,10 @@ class Song
   attr_accessor :name, :artist
   def initialize(name)
 # initialize with name 
+    @name = name
   end 
 # create class method .new_by_filename
-
+  def self.new_by_filename(file_name)
+    song = Song.new(file_name)
+  end
 end 
