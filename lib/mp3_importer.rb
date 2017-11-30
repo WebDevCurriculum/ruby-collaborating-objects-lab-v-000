7359e12b1e331c79c9525968955f6f66e87ae8ed
charityyoungblood <youngblood.charity@gmail.com>
2017-11-29 17:45:58 -0800

    added path method and @music_path instance variable to mp3_importer.rb

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,8 +1,17 @@
 require 'pry'
 # create MP3Importer class 
-  class MP3Importer 
+  class MP3Importer
+    attr_accessor :music_path
 # create initialize method w/ argument (music_path)
-
+    def initialize(music_path)
+      @music_path = music_path
+    end
+    
+# create path method 
+    
+    def path 
+      @music_path
+    end 
 # create files method 
     # - what does this method do - displays the file_name/type but does not display the file path 
     # - what does this method return - a String of mp3 file 
