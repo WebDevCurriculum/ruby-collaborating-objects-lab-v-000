49bececd1182b872d7cdc5636636b095aea09442
Charity <Youngblood.charity@gmail.com>
2017-11-29 16:19:51 -0800

    added comments for how to structure code

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,4 +2,9 @@
 
 # create initialize method w/ argument (music_path)
 
-# 
+# create files method 
+    # - what does this method do - displays the file_name/type but does not display the file path 
+    # - what does this method return - a String of mp3 file 
+# create an import method 
+  # add files to @@all array 
+  # creates songs from a filenmae 
