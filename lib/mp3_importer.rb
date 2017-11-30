4fd80b71e8676e922bf5b65e2fb72efeb6fffda3
charityyoungblood <youngblood.charity@gmail.com>
2017-11-30 11:28:40 -0800

    renamed class variable in mp3_importer.rb

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,11 +2,11 @@ require 'pry'
 # create MP3Importer class 
   class MP3Importer
     attr_accessor :music_path
-    @@path = []
+    @@music_files = []
 # create initialize method w/ argument (music_path)
     def initialize(music_path)
       @music_path = music_path
-      @@path = []
+      @@music_files = []
     end
     
 # create path method 
@@ -15,7 +15,11 @@ require 'pry'
     end 
 # create files method 
     def files
-      @@path << self.music_path
+     # add all files from mp3 folder to @@path array
+      # access files from mp3 folder
+      # add to @@path array
+      binding.pry
+      @@music_files << self.music_path
     # - what does this method do - displays the file_name/type but does not display the file path 
     # - what does this method return - a String of mp3 file 
     end
