5ecf89caa965f05f341d21e3147602d1b362b550
charityyoungblood <youngblood.charity@gmail.com>
2017-11-30 12:46:35 -0800

    organized code

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -17,16 +17,9 @@ require 'pry'
     def files
       # Google - list of file names from directory  - 
       # Get file names from mp3 folder 
-      # add to @@music_files array
       # print filename only without path
-      binding.pry
-      
       mp3_music = Dir.entries(@music_path).reject{|entry| entry == "." || entry == ".."}
-      @@music_files << mp3_music
-     
-      #@@music_files << self.music_path
-    # - what does this method do - displays the file_name/type but does not display the file path 
-    # - what does this method return - a String of mp3 file 
+      mp3_music
     end
 # create an import method 
   # add files to @@all array 
